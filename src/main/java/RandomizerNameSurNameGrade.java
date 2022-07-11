1eec96773088cd47aa3481ea8343a40f07962e6d
Yauhen Shviatsou <demum1930@gmail.com>
2022-07-12 00:34:13 +0300

    Feat: add Student,Runner,Randomizer

---
+++ src/main/java/RandomizerNameSurNameGrade.java
@@ -0,0 +1,23 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
+
+public class RandomizerNameSurNameGrade {
+
+    public String randomName (){
+        String firstLetterName = RandomStringUtils.randomAlphabetic(1).toUpperCase();
+        String remainingLettersName = RandomStringUtils.randomAlphabetic(3,10).toLowerCase();
+     return    firstLetterName+remainingLettersName;
+    }
+
+    public String randomSurname (){
+        String firstLetterSurName = RandomStringUtils.randomAlphabetic(1).toUpperCase();
+        String remainingLettersSurName = RandomStringUtils.randomAlphabetic(3,10).toLowerCase();
+        return firstLetterSurName+remainingLettersSurName;
+    }
+
+    public int randomGrade (){
+        String remainingLettersSurName = RandomStringUtils.random(1,false,true);
+        return Integer.parseInt(remainingLettersSurName);
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        RandomizerNameSurNameGrade randomizerNameSurNameGrade = new RandomizerNameSurNameGrade();
+        Student student1 = new Student(randomizerNameSurNameGrade.randomName(), randomizerNameSurNameGrade.randomSurname(), randomizerNameSurNameGrade.randomGrade(), "1");
+        Student student2 = new Student(randomizerNameSurNameGrade.randomName(), randomizerNameSurNameGrade.randomSurname(), randomizerNameSurNameGrade.randomGrade(), "2");
+        Student student3 = new Student(randomizerNameSurNameGrade.randomName(), randomizerNameSurNameGrade.randomSurname(), randomizerNameSurNameGrade.randomGrade(), "1");
+        Student student4 = new Student(randomizerNameSurNameGrade.randomName(), randomizerNameSurNameGrade.randomSurname(), randomizerNameSurNameGrade.randomGrade(), "1");
+
+        List<Student> list = new ArrayList<>();
+        list.add(student1);
+        list.add(student2);
+        list.add(student3);
+        list.add(student4);
+        ArrayList<Student> list2 = new ArrayList<>();
+
+         for( Student student:list)
+            if (student.getClassName().equals("1")) {
+                list2.add(student);
+            }
+
+        int classGrade = 0;
+      //  for (int i = 0; i < list2.size(); i++) {
+        for( Student student:list2){
+            classGrade +=student.getGrade();
+        }
+        classGrade = Math.round(classGrade / list2.size());
+        System.out.println(list2);
+        System.out.println("classGrade is: " + classGrade);
+    }
+}
